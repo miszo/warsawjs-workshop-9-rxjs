047a66639494197eaa30908f1e74c87e77ea0d03
Miszo Radomski <miszo@icloud.com>
2017-08-20 14:20:45 +0200

    promise example

---
+++ src/promise/index.js
@@ -0,0 +1,20 @@
+function fetchData(url) {
+  return new Promise(function(resolve, reject) {
+    const httpReq = new XMLHttpRequest();
+    httpReq.open('get', url, true)
+    httpReq.addEventListener('load', function() {
+      resolve(httpReq.response);
+    });
+    httpReq.addEventListener('error', function(err) {
+      reject(error);
+    });
+    httpReq.send();
+  });
+}
+
+fetchData('https://jsonplaceholder.typicode.com/posts')
+  .then(data => {
+    console.log(data);
+  }).catch(err => {
+    console.error(err);
+  });
